e6b897b693912fe60aa06acb1ae4107cef4a5aec
Lorenzo <lorenzonicotera02@gmail.com>
2024-12-18 15:20:01 +0100

    first phase of pong working

--- srcs/frontend/js/gameLoop.js
+++ srcs/frontend/js/gameLoop.js
@@ -4,7 +4,8 @@ import { Paddle } from './paddle.js';
 export function gameLoop(canvas) {
   const ctx = canvas.getContext('2d');
   const ball = new Ball(canvas.width / 2, canvas.height / 2, 10, 4, 3, canvas);
-  const paddle = new Paddle(canvas);
+  const paddle1 = new Paddle(canvas, 10, 'w', 's');
+  const paddle2 = new Paddle(canvas, canvas.width - 20, 'ArrowUp', 'ArrowDown');
 
   function update() {
     // Clear canvas
@@ -14,16 +15,16 @@ export function gameLoop(canvas) {
     ball.draw();
     ball.move();
 
-    // Draw paddle
-    paddle.draw();
+    // Draw paddles
+    paddle1.draw();
+    paddle2.draw();
 
-    // Check for paddle collision
+    // Check for paddle collisions
     if (
-      ball.y + ball.radius > canvas.height - paddle.height - 10 &&
-      ball.x > paddle.x &&
-      ball.x < paddle.x + paddle.width
+      (ball.x - ball.radius < paddle1.x + paddle1.width && ball.y > paddle1.y && ball.y < paddle1.y + paddle1.height) ||
+      (ball.x + ball.radius > paddle2.x && ball.y > paddle2.y && ball.y < paddle2.y + paddle2.height)
     ) {
-      ball.speedY = -ball.speedY;
+      ball.speedX = -ball.speedX;
     }
 
     // Request next frame
